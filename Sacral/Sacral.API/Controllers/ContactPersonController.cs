b18d87a2b2a93cf7dacf13bf6cb0ddc2a1fe801e
dotnet-code <@dotnet-code>
2023-06-13 15:44:50 +0000

    commit from code202306134451

---
+++ Sacral/Sacral.API/Controllers/ContactPersonController.cs
@@ -0,0 +1,21 @@
+﻿namespace Sacral.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactPersonController : ControllerBase
+    {
+        private readonly ContactPersonService _contactPersonService;
+        public ContactPersonController(ContactPersonService contactPersonService)
+        {
+            _contactPersonService = contactPersonService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContactPerson(ContactPersonDTO contactPersonDTO)
+        {
+            var id = await _contactPersonService.CreateContactPerson(contactPersonDTO);
+
+            return Ok(id);
+        }
+    }
+}
---
+++ Sacral/Sacral.Service/ContactPersonService.cs
@@ -0,0 +1,27 @@
+﻿using Sacral.DataAccess;
+using Sacral.DTO;
+
+namespace Sacral.Service
+{
+    public class ContactPersonService : IContactPersonService
+    {
+        public async Task<int> CreateContactPerson(ContactPersonDTO contactPersonDTO)
+        {
+            using(var dbContext = new SacralContext())
+            {
+                var contactPerson = new ContactPerson
+                {
+                    FirstName = contactPersonDTO.FirstName,
+                    LastName = contactPersonDTO.LastName, 
+                    Email = contactPersonDTO.Email,
+                    Phone = contactPersonDTO.Phone
+                };
+
+                dbContext.ContactPersons.Add(contactPerson);
+                await dbContext.SaveChangesAsync();
+
+                return contactPerson.Id;
+            }
+        }
+    }
+}
